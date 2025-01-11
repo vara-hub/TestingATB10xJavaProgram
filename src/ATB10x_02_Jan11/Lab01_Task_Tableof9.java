b771168c48a83ba73b651c3be769383c43b99827
vara-hub <varalaksmi.d@gmail.com>
2025-01-11 17:53:57 +0530

    Jan11

---
+++ src/ATB10x_02_Jan11/Lab01_Task_Tableof9.java
@@ -0,0 +1,22 @@
+package ATB10x_02_Jan11;
+
+public class Lab01_Task_Tableof9 {
+    public static void main(String[] args) {
+        int s = 9;
+        System.out.println("The Multiplication Table of 9:");
+        System.out.printf("9 * 1 = %d%n",s*1);
+        System.out.printf("9 * 2 = %d%n",s*2);
+        System.out.printf("9 * 3 = %d%n",s*3);
+        System.out.printf("9 * 4 = %d%n",s*4);
+        System.out.printf("9 * 5 = %d%n",s*5);
+        System.out.printf("9 * 6 = %d%n",s*6);
+        System.out.printf("9 * 7 = %d%n",s*7);
+        System.out.printf("9 * 8 = %d%n",s*8);
+        System.out.printf("9 * 9 = %d%n",s*9);
+        System.out.printf("9 * 10 = %d%n",s*10);
+        System.out.printf("9 * 11 = %d%n",s*11);
+        System.out.printf("9 * 12 = %d%n",s*12);
+        System.out.printf("s * 13 = %d%n",s*13);
+    }
+
+}
